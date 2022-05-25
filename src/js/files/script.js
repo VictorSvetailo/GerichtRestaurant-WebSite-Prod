f106f0601943016928cb2390d00ea5e1b82aef73
Victor <victorioeve@gmail.com>
2022-05-25 19:59:11 +0300

    Устранил баг с параллаксом и header menu. Пришлось создавать новый скрипт для параллакса так как в старом не разобрался

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -142,3 +142,73 @@ document.addEventListener('DOMContentLoaded', function () {
 	// 	scrollTo(0, 400);
 	// }
 });
+
+// ========================================================================================================================================================
+
+window.onload = function () {
+	const parallax = document.querySelector('.welcome');
+
+	if (parallax) {
+		const content = document.querySelector('.welcome__container');
+		const images = document.querySelector('.images-welcome__background');
+
+		// Коэффициенты
+		const forImages = 40;
+
+		// Скорость анимации
+		const speed = 0.07;
+
+		// Объявление переменных
+		let positionX = 0,
+			positionY = 0;
+		let coordXprocent = 0,
+			coordYprocent = 0;
+
+		function setMouseParallaxStyle() {
+			const distX = coordXprocent - positionX;
+			const distY = coordYprocent - positionY;
+			positionX = positionX + (distX * speed);
+			positionY = positionY + (distY * speed);
+
+			// Передаем стили
+			images.style.cssText = `transform: translate(${positionX / forImages}%,${positionY / forImages}%);`
+			requestAnimationFrame(setMouseParallaxStyle);
+		}
+		setMouseParallaxStyle();
+
+		parallax.addEventListener("mousemove", function (e) {
+			// Получение ширины и высоты блока
+			const parallaxWidth = parallax.offsetWidth;
+			const parallaxHeight = parallax.offsetHeight;
+
+			// Ноль по середине
+			const coordX = e.pageX - parallaxWidth / 2;
+			const coordY = e.pageY - parallaxHeight / 2;
+
+			// Получаем проценты
+			coordXprocent = coordX / parallaxWidth * 100;
+			coordYprocent = coordY / parallaxHeight * 100;
+		});
+
+		// Parallax при скролле
+
+		let thresholdSets = [];
+		for (let i = 0; i <= 1.0; i += 0.005) {
+			thresholdSets.push(i);
+		}
+		const callback = function (entries, observer) {
+			const scrollTopProcent = window.pageYOffset / parallax.offsetHeight * 100;
+			setParallaxItemsStyle(scrollTopProcent);
+		};
+		const observer = new IntersectionObserver(callback, {
+			threshold: thresholdSets
+		});
+
+		observer.observe(document.querySelector('.history'));
+
+		function setParallaxItemsStyle(scrollTopProcent) {
+			content.style.cssText = `transform: translate(0%,-${scrollTopProcent / 1}%);`;
+			images.parentElement.style.cssText = `transform: translate(0%,-${scrollTopPercent / 1}%);`;
+		}
+	}
+}
